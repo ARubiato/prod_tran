ad4a6b9c957f38af9e0be0b48465ccd9a3f8916a
ARubiato <alessanderrubiato@yahoo.com>
2018-08-15 00:48:39 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel = "stylesheet" href = "css/uikit.min.css">
+	<script src="js/uikit.min.js"></script>
+    <script src="js/uikit-icons.min.js"></script>
+</head>
+<body>
+	<div class = "uk-container bg-info">
+		<div class = "uk-card">
+			<div class = "uk-card-header">
+				<div class = "uk-card-title">
+				<h4>"SOME SYSTEM TITLE" MANAGEMENT SYSTEM</h4>
+				</div>
+				<hr>
+
+				<nav class = "uk-navbar-container" uk-navbar="mode:click">
+					<div class = "uk-navbar-left">
+						<ul class = "uk-navbar-nav">
+							<li>
+								<a href = "#">Products</a>
+								<div class = "uk-navbar-dropdown">
+									<ul class = "uk-nav uk-navbar-dropdown-nav">
+										<li><a href = "#">Add products</a></li>
+										<li><a href = "#">View products</a></li>
+									</ul>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</nav>
+			</div>
+		</div>
+	</div>
+
+			<div class = "uk-card-body">
+				<form>
+					<legend class = "uk-legend">Add a product</legend>
+						<div class = "uk-margin">
+							<input class = "uk-input uk-form-width-large" type="text" name="" placeholder="Product name" required>
+						</div>
+				</form>
+			</div>
+
+		</div>
+
+
+	</div>
+
+</body>
+</html>
