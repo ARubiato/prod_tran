daf8b784cf1ae2c0f5e57df96aa913faa44b7451
unknown <alessanderrubiato@yahoo.com>
2018-08-16 15:48:13 +0800

    added a get method

--- src/edit_prd.php
+++ src/edit_prd.php
@@ -1,43 +1,41 @@
-
 <?php require ("header.php");
 	  require ("..\php\server.php");
 
-	$vquery = "SELECT * FROM products ORDER BY P_NAME";
-	$results = mysqli_query($conn,$vquery);
 
-	if(isset($_GET['P_ID'])) {
-		$P_ID = $_GET['P_ID'];
+if ($_GET['id']) {
+$P_ID = $_GET['id'];
 
-		$query = "SELECT * from products WHERE P_ID = {$P_ID}";
-		$results = mysqli_query($conn,$query);
-		$data = mysqli_fetch_assoc($results);
-
+$query = "SELECT * FROM products WHERE P_ID = '$P_ID'";
+$results = mysqli_query($conn,$query);
+$data = mysqli_fetch_assoc($results);
 
 }
-	
-	?>
+ 
+
+
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Products</title>
-	
+		
 </head>
 <body>
 	<div class = "uk-container">
 		<div class = "uk-card">
-			
-			<div class = "uk-card-body uk-card-muted">
-				<form method = "post" action = "update_prd.php">
+			<div class = "uk-card-body uk-card-muted"> 
+				<form method = "post" action = "..\php\update_prd.php">
 					<legend class = "uk-legend">Edit a product</legend>
 						<div class = "uk-margin">
-							<input class = "uk-input uk-form-width-large" type="text" name="P_NAME" placeholder= "Product name" value = "<?php echo $data['P_NAME']; ?>" required>
+							<input class = "uk-input uk-form-width-large" type="text" name="P_NAME" placeholder= "Product name" value = "<?php echo $data['P_NAME']?>" required>
 						</div>
 						<div class = "uk-margin">
-							<input class = "uk-input uk-form-width-large" type = "text" name = "P_PRICE" placeholder = "Price" value = "<?php echo $data['P_Price'];?>"required>
+							<input class = "uk-input uk-form-width-large" type = "text" name = "P_PRICE" placeholder = "Price" value = "<?php echo isset($data['P_PRICE']) ? $data['P_PRICE'] : ''; ?>"required>
 						</div>
-
 						<div class = "uk-margin">
-							<button class="uk-button uk-button-primary" type="submit" name = "submit">Submit</button>
+							<input type="hidden" name="P_ID" value="<?php echo $data['P_ID'] ?>" />
+							<button class="uk-button uk-button-primary" type="submit" name = "update">Submit</button>
 						</div>
 				</form>
 
